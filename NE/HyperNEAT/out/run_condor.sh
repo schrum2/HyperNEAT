db207048eef71622ed259e7ea9d3ee00ff4c3dd0
Matthew Hausknecht <matthew.hausknecht@gmail.com>
2012-02-01 12:03:42 -0600

    Added bash shell script to run the program.

---
+++ NE/HyperNEAT/out/run_condor.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+LOG="expr.log"
+date > $LOG
+echo "Starting Experimental Runs..."
+echo "Writing to logfile $LOG"
+echo "Starting Experimental Runs..." >> $LOG
+echo "Current Git CheckPt: " >> $LOG
+git rev-parse HEAD >> $LOG
+
+# Experiments Here
+for i in {1..1}
+do
+	./condor_run.rb /u/mhauskn/projects/HyperNEAT/NE/HyperNEAT/out/ 250 100
+done
+
+echo "Experiments Finished!"
+echo "Experiments Finished!" >> $LOG
+date >> $LOG
