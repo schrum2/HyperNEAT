bf88c4614580f0ab4034d53735793ec8f0b4d990
Matthew Hausknecht <matthew.hausknecht@gmail.com>
2012-02-28 16:36:21 -0600

    in hte process of cutting down the self detection agent.

--- NE/HyperNEAT/ale_v0.1/src/common/module.mk
+++ NE/HyperNEAT/ale_v0.1/src/common/module.mk
@@ -4,6 +4,7 @@ MODULE_OBJS := \
 	src/common/SoundNull.o \
 	src/common/export_screen.o \
 	src/common/display_screen.o \
+	src/common/visual_processor.o \
 	src/common/vector_matrix_tools.o \
 
 MODULE_DIRS += \
