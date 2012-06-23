ac9dce572a713262c317b69f4c860f75a0f549e0
Matthew Hausknecht <matthew.hausknecht@gmail.com>
2012-06-22 23:29:45 -0500

    Modified the interface file to build correctly.

--- NE/HyperNEAT/ale_v0.1/src/interface.hpp
+++ NE/HyperNEAT/ale_v0.1/src/interface.hpp
@@ -1,5 +1,5 @@
-#ifndef ATARI_INTERFACE_H
-#define ATARI_INTERFACE_H
+#ifndef ALE_INTERFACE_H
+#define ALE_INTERFACE_H
 
 #include <cstdlib>
 #include <ctime> 
@@ -19,122 +19,126 @@
 #include "player_agents/player_agent.h"
 #include "player_agents/self_detection_agent.h"
 
-string str_ver = "0.1";
-string str_welcome = "A.L.E: Atari 2600 Learning Environment (version " + str_ver + ")\n"
-+ "[Powered by Stella]\n"
-+ "Use -help for help screen.";
-OSystem* theOSystem = (OSystem*) NULL;
-InternalController* p_game_controllr  = NULL;
+class ALEInterface
+{
+public:
+    OSystem* theOSystem;
+    InternalController* game_controller;
 
-static size_t time_start; 
-static size_t time_end; 
+    static size_t time_start; 
+    static size_t time_end; 
 
-void cleanup() {
-  // Does general Cleanup in case any operation failed (or at end of program)
-  if(theOSystem) {
-    delete theOSystem;
-  }
+public:
+    ALEInterface(string rom_file, bool display_screen): theOSystem(NULL), game_controller(NULL) {
+        int argc = 6;
+        char** argv = new char*[argc];
+        for (int i=0; i<=argc; i++) {
+            argv[i] = new char[200];
+        }
+        strcpy(argv[0],"./ale");
+        strcpy(argv[1],"-player_agent");
+        strcpy(argv[2],"self_detection_agent");
+        strcpy(argv[3],"-display_screen");
+        if (display_screen)
+            strcpy(argv[4],"true");
+        else
+            strcpy(argv[4],"false");
 
-  if(p_game_controllr) {
-    delete p_game_controllr;
-  }
-}
+        strcpy(argv[5],rom_file.c_str());  
 
-void initializeEmulator(string rom_file, bool display_screen) {
-  int argc = 6;
-  char** argv = new char*[argc];
-  for (int i=0; i<=argc; i++) {
-    argv[i] = new char[200];
-  }
-  strcpy(argv[0],"./ale");
-  strcpy(argv[1],"-player_agent");
-  strcpy(argv[2],"self_detection_agent");
-  strcpy(argv[3],"-display_screen");
-  if (display_screen)
-    strcpy(argv[4],"true");
-  else
-    strcpy(argv[4],"false");
+        string str_ver = "0.1";
+        string str_welcome = "A.L.E: Atari 2600 Learning Environment (version " + str_ver + ")\n"
+            + "[Powered by Stella]\n"
+            + "Use -help for help screen.";
 
-  //strcpy(argv[5],"../ale_v0.1/roms/asterix.bin");
-  strcpy(argv[5],rom_file.c_str());  
+        cout << str_welcome << endl;
+        theOSystem = new OSystemUNIX();
+        SettingsUNIX settings(theOSystem);
+        theOSystem->settings().loadConfig();
 
-  cout << str_welcome << endl;
-  theOSystem = new OSystemUNIX();
-  SettingsUNIX settings(theOSystem);
-  theOSystem->settings().loadConfig();
+        // Load the RL parameters
+        string rl_params_loc = theOSystem->settings().getString("working_dir") +
+            theOSystem->settings().getString("rl_params_file");
+        theOSystem->settings().loadConfig(rl_params_loc.c_str());
 
-  // Load the RL parameters
-  string rl_params_loc = theOSystem->settings().getString("working_dir") +
-    theOSystem->settings().getString("rl_params_file");
-  theOSystem->settings().loadConfig(rl_params_loc.c_str());
+        // Load the Class Discovery parameters
+        string cl_dis_params_loc = theOSystem->settings().getString("working_dir") +
+            theOSystem->settings().getString("class_disc_params_file");
+        theOSystem->settings().loadConfig(cl_dis_params_loc.c_str());
 
-  // Load the Class Discovery parameters
-  string cl_dis_params_loc = theOSystem->settings().getString("working_dir") +
-    theOSystem->settings().getString("class_disc_params_file");
-  theOSystem->settings().loadConfig(cl_dis_params_loc.c_str());
+        // Load the Search-Agent parameters
+        string search_params_loc = theOSystem->settings().getString("working_dir") +
+            "search_params.txt";
+        theOSystem->settings().loadConfig(search_params_loc.c_str());
 
-  // Load the Search-Agent parameters
-  string search_params_loc = theOSystem->settings().getString("working_dir") +
-    "search_params.txt";
-  theOSystem->settings().loadConfig(search_params_loc.c_str());
+        // Load the Experiment parameters
+        string exp_params_loc = theOSystem->settings().getString("working_dir") +
+            "experiment_params.txt";
+        theOSystem->settings().loadConfig(exp_params_loc.c_str());
 
-  // Load the Experiment parameters
-  string exp_params_loc = theOSystem->settings().getString("working_dir") +
-    "experiment_params.txt";
-  theOSystem->settings().loadConfig(exp_params_loc.c_str());
+        // Take care of commandline arguments (over-ride all file settings)
+        string romfile = theOSystem->settings().loadCommandLine(argc, argv);
 
-  // Take care of commandline arguments (over-ride all file settings)
-  string romfile = theOSystem->settings().loadCommandLine(argc, argv);
+        // Finally, make sure the settings are valid
+        // We do it once here, so the rest of the program can assume valid settings
+        theOSystem->settings().validate();
 
-  // Finally, make sure the settings are valid
-  // We do it once here, so the rest of the program can assume valid settings
-  theOSystem->settings().validate();
+        // Create the full OSystem after the settings, since settings are
+        // probably needed for defaults
+        theOSystem->create();
 
-  // Create the full OSystem after the settings, since settings are
-  // probably needed for defaults
-  theOSystem->create();
+        //// Main loop ////
+        // First we check if a ROM is specified on the commandline.  If so, and if
+        //   the ROM actually exists, use it to create a new console.
+        // If not, use the built-in ROM launcher.  In this case, we enter 'launcher'
+        //   mode and let the main event loop take care of opening a new console/ROM.
+        if(argc == 1 || romfile == "" || !FilesystemNode::fileExists(romfile)) {
+            printf("No ROM File specified or the ROM file was not found.\n");
+            return;
+        } else if(theOSystem->createConsole(romfile)) 	{
+            printf("Running ROM file...\n");
+            theOSystem->settings().setString("rom_file", romfile);
+        } else {
+            printf("Unable to create console from ROM file.\n");
+            return;
+        }
 
-  //// Main loop ////
-  // First we check if a ROM is specified on the commandline.  If so, and if
-  //   the ROM actually exists, use it to create a new console.
-  // If not, use the built-in ROM launcher.  In this case, we enter 'launcher'
-  //   mode and let the main event loop take care of opening a new console/ROM.
-  if(argc == 1 || romfile == "" || !FilesystemNode::fileExists(romfile)) {
-    printf("No ROM File specified or the ROM file was not found.\n");
-    return;
-  } else if(theOSystem->createConsole(romfile)) 	{
-    printf("Running ROM file...\n");
-    theOSystem->settings().setString("rom_file", romfile);
-  } else {
-    cleanup();
-    return;
-  }
+        // Seed the Random number generator
+        if (theOSystem->settings().getString("random_seed") == "time") {
+            cout << "Random Seed: Time" << endl;
+            srand((unsigned)time(0)); 
+            srand48((unsigned)time(0));
+        } else {
+            int seed = theOSystem->settings().getInt("random_seed");
+            assert(seed >= 0);
+            cout << "Random Seed: " << seed << endl;
+            srand((unsigned)seed); 
+            srand48((unsigned)seed);
+        }
 
-  // Seed the Random number generator
-  if (theOSystem->settings().getString("random_seed") == "time") {
-    cout << "Random Seed: Time" << endl;
-    srand((unsigned)time(0)); 
-    srand48((unsigned)time(0));
-  } else {
-    int seed = theOSystem->settings().getInt("random_seed");
-    assert(seed >= 0);
-    cout << "Random Seed: " << seed << endl;
-    srand((unsigned)seed); 
-    srand48((unsigned)seed);
-  }
+        // Generate the GameController
+        game_controller = new InternalController(theOSystem);
+        theOSystem->setGameController(game_controller);
+        cout << "Games will be controlled internally, " << 
+            "through the assigned player Agent" << endl;
 
-  // Generate the GameController
-  p_game_controllr = new InternalController(theOSystem);
-  theOSystem->setGameController(p_game_controllr);
-  cout << "Games will be controlled internally, " << 
-      "through the assigned player Agent" << endl;
+        // Set the Pallete 
+        theOSystem->console().setPalette("standard");
+    }
 
-  // Set the Pallete 
-  theOSystem->console().setPalette("standard");
-}
+    ~ALEInterface() {
+        if (theOSystem) {
+            delete theOSystem;
+        }
 
-void display_screen(const IntMatrix& pm_screen_matrix) {
-    p_game_controllr->getPlayerAgentLeft()->display_screen(pm_screen_matrix);
+        if (game_controller) {
+            delete game_controller;
+        }
+    }
+
+    void display_screen(const IntMatrix& pm_screen_matrix) {
+        game_controller->getPlayerAgentLeft()->display_screen(pm_screen_matrix);
+    }
 };
 
 #endif
