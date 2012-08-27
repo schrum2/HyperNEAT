a061ce8d4212f32dddaf4aa1afc3906a2890b550
Matthew Hausknecht <matthew.hausknecht@gmail.com>
2012-08-27 16:48:28 -0500

    Removing ale. Will replace with a submodule.

--- ale_v0.1/module.mk
+++
@@ -1,9 +0,0 @@
-MODULE := src/
-
-MODULE_OBJS := \
-#	test.o
-
-MODULE_DIRS += \
-
-# Include common rules 
-include $(srcdir)/common.rules
--- ale_v0.1/src/player_agents/models/baseline_model.cpp
+++
@@ -1,6 +0,0 @@
-#include "baseline_model.h"
-
-void BaselineModel::update(IntArr* state, Action action, float reward, IntArr* nextState) {};
-void BaselineModel::predict(IntArr* state, Action action, float* reward, IntArr* _nextState) {
-  (*_nextState) = (*state);
-};
