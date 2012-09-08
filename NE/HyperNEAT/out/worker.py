650ec6a6db4467dd417321c6dd1e3d1d5d5e4225
Matthew Hausknecht <matthew.hausknecht@gmail.com>
2012-09-07 21:07:41 -0500

    finished writing the worker.

--- NE/HyperNEAT/out/worker.py
+++ NE/HyperNEAT/out/worker.py
@@ -11,22 +11,59 @@
 #        \   \  '---;\
 #        |`\  \      \\
 #       / / \  \      \\
-#     _/ /   / /      _\\/
+#     _/ /   / /      _\\_
 #    ( \/   /_/       \   |
 #     \_)  (___)       '._/
 #--------------------------------
 
-import argparse
+import argparse, os, random, time
 
+# This runs a single Atari game.
+def run_game(executable, dataFile, generationFile, individualId, fitnessFile, rom):
+    from subprocess import call
+    call([executable, "-I", dataFile, "-P", generationFile, "-N",
+                     str(individualId), "-F", fitnessFile, "-G", rom])
+                    
 parser = argparse.ArgumentParser(description='Runs Atari games without tire.')
-parser.add_argument('-i', metavar='in-file', type=argparse.FileType('r'))
-parser.add_argument('-o', metavar='out-file', type=argparse.FileType('w'))
-parser.add_argument('-e', metavar='executable-file', type=argparse.FileType('r'))
-try:
-    results = parser.parse_args()
-    print 'Input file:', results.i
-    print 'Output file:', results.o
-    print 'Executable file:', results.e
-except IOError, msg:
-    parser.error(str(msg))
+parser.add_argument('-e', metavar='executable-file', help='This should point to atari_evaluate executable.')
+parser.add_argument('-r', metavar='results-dir',
+                    help='This should point to the directory that results are being accumulated in.')
+parser.add_argument('-d', metavar='data-file', help='This should point to the AtariExperiment.dat file')
+parser.add_argument('-n', metavar='num-individuals', help='This is the number of individuals in each generation.',
+                    type=int)
+parser.add_argument('-g', metavar='num-generations',
+                    help='This is the number of generats before the run is complete.',type=int);
+parser.add_argument('-G', metavar='rom-file', help='This should point to the rom to be run.')
 
+args = parser.parse_args()
+rom                      = args.G
+executable               = args.e
+dataFile                 = args.d
+maxGeneration            = args.g
+resultsDir               = args.r
+individualsPerGeneration = args.n
+
+currentGeneration = 0
+while currentGeneration < maxGeneration:
+    # If an eval file for this generation already exists, go to next gen
+    evalFile = "generation" + str(currentGeneration) + ".eval.xml.gz"
+    if os.path.exists(os.path.join(resultsDir,evalFile)):
+        currentGeneration += 1
+        continue
+
+    generationFile = "generation" + str(currentGeneration) + ".xml.gz"
+    generationPath = os.path.join(resultsDir,generationFile)
+
+    # Look for fitness files which indicate that games are being run
+    individualIds = range(individualsPerGeneration)
+    random.shuffle(individualIds)
+    for individualId in individualIds:
+        fitnessFile = "fitness"+str(currentGeneration)+"."+str(individualId)
+        fitnessPath = os.path.join(resultsDir,fitnessFile)
+        if os.path.exists(fitnessPath):
+            continue
+        open(fitnessPath,'w').close() # Touch this fitness file
+        run_game(executable, dataFile, generationPath, individualId, fitnessPath, rom)
+
+    # At this point we just need to wait for the master to create the next generation
+    time.sleep(.1)
