4ffb5b307a54bf0036e957f1a5d61ab5ea4df97c
Matthew Hausknecht <matthew.hausknecht@gmail.com>
2012-11-20 01:41:59 -0600

    Modified plot.py to show standard error bars.

--- NE/HyperNEAT/out/plot.py
+++ NE/HyperNEAT/out/plot.py
@@ -6,20 +6,25 @@ if len(sys.argv) < 2:
     print 'Usage:',sys.argv[0],'nohup.out'
     sys.exit(0)
 
-avgFit, champFit = [],[]
+avgFit, champFit, stdErr = [],[],[]
 f = open(sys.argv[1],'r')
 for line in f:
     if str.startswith(line,'Champion fitness'):
         champFit.append(int(line.split()[2]))
     elif 'overall_average' in line:
         avgFit.append(float(line.split()[4]))
+    elif 'StandardError' in line:
+        stdErr.append(float(line.split()[-1]))
 f.close()
 
 import matplotlib.pyplot as plt
 
 x1 = range(len(champFit))
 x2 = range(len(avgFit))
+
 plt.plot(x1,champFit,x2,avgFit)
+plt.plot(x2,[avgFit[i] + stdErr[i] for i in x2],'g:')
+plt.plot(x2,[avgFit[i] - stdErr[i] for i in x2],'g:')
 plt.xlabel('Generation')
 plt.ylabel('Fitness')
 plt.show()
